bc8d0ef1ef5e4be65d8b7732c2482280137e0a3c
Chrispear <li.chrispear@gmail.com>
2026-09-16 22:11:50 +0800

    table structure for "user"

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from app.db.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique = True, index=True, nullable = False)
+    email = Column(String, unique = True, index = True, nullable = False)
+    password_hash = Column(String, nullable = False)
+    is_active = Column(Boolean, default = True)
+    created_at = Column(DateTime, default = datetime.utcnow)
